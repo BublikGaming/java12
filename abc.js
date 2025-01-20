fd4eef3c5d75b6d9cb00355fa92561b1ce15b5c7
Золотенков Даниил Алексеевич <d.zolotenkov@mkit.local>
2025-01-20 11:41:17 +0300

    project init2

--- abc.js
+++ abc.js
@@ -1,3 +1,9 @@
+/#1/
 let a = 5,b = 10, c = 3;
 let result1 = a + b - c;
-console.log(`5 + 10 - 3 = ${result1}`)
+console.log(`5 + 10 - 3 = ${result1}`);
+/#2/
+let a1 = 10, b1 = 20, c1 = 41;
+let result2 = (a1 + b1) / c1;
+console.log(`result = ${result2}`);
+
