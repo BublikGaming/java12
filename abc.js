a450e945b3ea155d461c8a0a2eaaaf3c035ce17e
Золотенков Даниил Алексеевич <d.zolotenkov@mkit.local>
2025-01-20 12:11:50 +0300

    add 1

--- abc.js
+++ abc.js
@@ -1,3 +1,4 @@
+
 /#1/
 let a = 5,b = 10, c = 3;
 let result1 = a + b - c;
@@ -5,5 +6,14 @@ console.log(`5 + 10 - 3 = ${result1}`);
 /#2/
 let a1 = 10, b1 = 20, c1 = 41;
 let result2 = (a1 + b1) / c1;
-console.log(`result = ${result2}`);
-
+console.log(`result = ${result2}`); 
+/#3/
+let b2 = 14,a2 = 9;
+function proverka(a2,b2)
+{
+if (a2 % 2) {console.log(`Четное`);}
+    else {console.log(`Нечетное`)}
+    if (b2 % 2) {console.log(`Четное`);}
+    else {console.log(`Нечетное`)}
+}
+console.log(proverka(a2,b2));
